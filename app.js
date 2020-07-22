9c331aa82870a79a2c35cc6f0f466a778ec5ef1f
Ian Hooper <ianhooper8@gmail.com>
2020-07-22 13:47:45 -0400

    restarting by adding basic questions

--- app.js
+++ app.js
@@ -1,124 +1,47 @@
-
-var counter = 10;
-var interval = setInterval(function() {
-    counter--;
-    // Display 'counter' wherever you want to display it.
-    if (counter <= 0) {
-     		clearInterval(interval);
-      	$('#timer').html("<h3>Count down complete</h3>");  
-        return;
-    }else{
-    	$('#time').text(counter);
-      console.log("Timer --> " + counter);
-    }
-}, 1000);
-
-function Question(text, choices, answer) {
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
+var question1 = {
+    question: "Commonly used data types do not include ______.",
+    answer1: "strings",
+    answer2: "boolians",
+    answer3: "alerts",
+    answer4: "numbers",
+    rightAnswer: "numbers"
 }
 
-Question.prototype.correctAnswer = function(choice) {
-    return choice === this.answer;
+var question2 = {
+    question: "The condition in an if / else statement is enclosed within ______.",
+    answer1: "quotes",
+    answer2: "curly brackets",
+    answer3: "parentheses",
+    answer4: "square brackets",
+    rightAnswer: "parenthesis"
 }
 
-
-function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0;
+var question3 = {
+    question: "Arrays in JavaScript can be used to store ______.",
+    answer1: "numbers and strings",
+    answer2: "other arrays",
+    answer3: "boolians",
+    answer4: "all of the above",
+    rightAnswer: "all of the above"
 }
 
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions[this.questionIndex];
+var question4 = {
+    question: "String values must be enclosed within _____ when being assigned variables.",
+    answer1: "commas",
+    answer2: "curly brackets",
+    answer3: "quotes",
+    answer4: "parentheses",
+    rightAnswer: "quotes"
 }
 
-Quiz.prototype.isEnded = function() {
-    return this.questions.length === this.questionIndex;
+var question5 = {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer1: "JavaScript",
+    answer2: "terminal/bash",
+    answer3: "for loops",
+    answer4: "console log",
+    rightAnswer: "console log"
 }
 
-Quiz.prototype.guess = function(answer) {
+     
 
-    if(this.getQuestionIndex() .correctAnswer(answer)) {
-        this.score++;
-    }
-    this.questionIndex++;
-    
-}
-
-function populate() {
-    if(quiz.isEnded()) {
-        showScores();
-    }
-    
-    else {
-        var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
-    }
-        var choices = quiz.getQuestionIndex().choices;
-        for (var i = 0; i < choices.length; i++) {
-            var element = document.getElementById("choice" + i);
-            element.innerHTML = choices(i);
-            guess("btn" + i, choices[i]);
-        }
-
-        showProgress();
-
-    
-
-function guess(id, guess) {
-    var button = document.getElementById(id);
-    button.onclick = function() {
-        quiz.guess(guess);
-        populate();
-    }
-}
-
-
-function showProgress(){
-    var currentQuestionNumber = quiz.questionIndex + 1;
-    var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNumber + "of " + quiz.questionIndex.length;
-}
-
-function showScores() {
-    var gameOverHtml = "<h1>Result</h1>";
-        gameOverHtml += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-            var element = document.getElementById("quiz");
-                element.innerHTML = gameOverHtml;
-            }
-
-
-var questions = [
-    new Question("The condition in an if / else statement is enclosed within ______.", ["quotes","curly brackets","parentheses","square brackets"], "parenthesis"),
-    new Question("The condition in an if / else statement is enclosed within ______.", ["quotes","curly brackets","parentheses","square brackets"], "parenthesis"),
-    new Question("Arrays in JavaScript can be used to store _____.", ["numbers and strings","other arrays","boolians","all of the above"], "all of the above"),
-    new Question("String values must be enclosed within _____ when being assigned variables.", ["commas","curly brackets","quotes","parentheses"], "quotes"),
-    new Question("A very useful tool used during development and debugging for printing content to the debugger is:", ["JavaScript","terminal/bash","for loops","console log"], "console log")
-];
-
-var quiz = new Quiz(questions);
-
-populate()
-}
-
-var answer = selectedOption.value;
-    if(question[currentQuestion].answer == answer) {
-        score + 2;
-    }
-    selectedOption.checked = false;
-    currentQuestion++;
-    if(currentQuestion == totQuestions - 1) {
-        nextButton.textContent = 'finished';
-    }
-    if(currentQuestion == totQuestions) {
-        container.style.display = 'none';
-        resultCont.style.display = '';
-        resultCont.textContent = 'Your score: ' + score;
-        return;
-    }
-    loadQuestion(currentQuestion);
-
-
-    loadQuestion(currentQuestion);
